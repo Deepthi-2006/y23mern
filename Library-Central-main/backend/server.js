1be7fc438c0c02855ae07375597603af80df1e82
Deepthi-2006 <163737846+Deepthi-2006@users.noreply.github.com>
2024-12-08 02:05:27 +0530

    Update server.js

--- Library-Central-main/backend/server.js
+++ Library-Central-main/backend/server.js
@@ -22,14 +22,13 @@ import authRoutes from './routes/auth.js';
 import librarianRoutes from './routes/librarian.js';
 import userRoutes from './routes/user.js';
 import sectionRoutes from './routes/SectionRoutes.js'
+import ebookRoutes from './routes/ebookRoutes.js';
 
 app.use('/api/auth', authRoutes);
 app.use('/api/librarian', librarianRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/section',sectionRoutes);
 const PORT = 5000;
-const ebookRoutes = require("./routes/ebookRoutes");
+
 app.use("/api/ebooks", ebookRoutes);
-
-
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
